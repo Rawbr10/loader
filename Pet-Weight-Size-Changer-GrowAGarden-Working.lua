e01f742ad308e691d994793cb7d8d380a8a19891
Roblox Scripts <gregyheport@gmail.com>
2025-08-21 07:07:00 +0800

    Update and rename pet-tool-size-changer-growagarden-working.lua to Pet-Weight-Size-Changer-GrowAGarden-Working.lua

---
+++ Pet-Weight-Size-Changer-GrowAGarden-Working.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Rawbr10/loader/refs/heads/main/pet.lua"))()
+task.wait(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Rawbr10/bloodball/refs/heads/main/loader2.lua"))()
+-- Dont Remove the second script it bypass the anti cheat if you remove it theres a chance you will get banned
