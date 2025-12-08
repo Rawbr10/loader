53442c5d4c9aeb7fac6c09ee9bf3e0bb099d2fca
Roblox Scripts <gregyheport@gmail.com>
2025-12-08 20:56:08 +0800

    Update loader.js

--- api/loader.js
+++ api/loader.js
@@ -8,26 +8,7 @@ export default function handler(req, res) {
         return res.redirect(302, 'https://api.luarmor.net/');
     }
     
-    const script = `
-
-    local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
-local RunService = game:GetService("RunService")
-
-local a;
-a = hookfunction(hookfunction, newcclosure(function(args, args2)
-    if args == game.HttpGet then
-    LocalPlayer:Kick("Why are you using http spy chill bro")
-    task.wait(0.15)
-    while true do 
-    LocalPlayer:Kick("Why are you using http spy chill bro")
-  end
-   return warn
-    end
-    return a(args, args2)
-end))
-
-loadstring(game:HttpGet("https://github.com/Rawbr10/loader/raw/refs/heads/main/ProjectX-Steal-A-Brainrot-Anti-Loader.lua"))()
+    const script = `loadstring(game:HttpGet("https://github.com/Rawbr10/loader/raw/refs/heads/main/ProjectX-Steal-A-Brainrot-Anti-Loader.lua"))()
 `;
     
     return res.status(200).send(script);
