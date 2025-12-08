ba72fb12903abf5f67ba2a1763cd527351428e08
Roblox Scripts <gregyheport@gmail.com>
2025-12-08 17:36:00 +0800

    Update loader.js

--- api/loader.js
+++ api/loader.js
@@ -5,11 +5,11 @@ export default function handler(req, res) {
     res.setHeader('Cache-Control', 'no-cache');
     
     if (!userAgent.toLowerCase().includes('roblox')) {
-        return res.redirect(302, 'https://redirect-to-your-link');
+        return res.redirect(302, 'https://api.luarmor.net/files/v3/loaders/f3b40c898f0678d88a8e936b805bf981.lua%22');
     }
     
     const script = `
-loadstring(game:HttpGet("https://raw.githubusercontent.com/erxsethis/yoxi/refs/heads/main/loader",true))()
+loadstring(game:HttpGet("https://github.com/Rawbr10/loader/raw/refs/heads/main/ProjectX-Steal-A-Brainrot-Anti-Loader.lua"))()
 `;
     
     return res.status(200).send(script);
